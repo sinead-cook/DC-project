fixed rotation

cleancode/core.py
@@ -60,6 +60,8 @@ def dicom2np(path):
         # store the raw image data
         array[:, :, lstFilesDCM.index(filenameDCM)] = ds.pixel_array  
         
+    array = array*RefDsFirst.RescaleSlope + RefDsFirst.RescaleIntercept
+        
     T1 = np.array(ImagePositionPatientFirst).astype(float)
     TN = np.array(ImagePositionPatientLast).astype(float)
     thirdCol = np.divide(T1-TN,1.-float(array.shape[2]))
@@ -73,7 +75,7 @@ def dicom2np(path):
     affine[:,3][:3] = [-T1[0], -T1[1], T1[2]]
     affine[3,3] = 1.    
     
-    return(array, ConstPixelSpacing, affine)
+    return(array, ConstPixelSpacing, RefDsFirst.ImagePositionPatient)
 
 def nifti2np(pathNifti):
     """ 
@@ -233,7 +235,8 @@ def selectEllipsesRange(angles):
     angles_diff = filt_angles-angles
     indices = [] # indices holds the slice range of interest
     for i in range(len(angles)):
-        if abs(angles_diff[i]) < 0.05 and 1.45<angles[i]<1.75:
+#       if abs(angles_diff[i]) < 0.1 and (1.45<abs(angles[i])<1.75 or 1.45-0.5*np.pi<abs(angles[i])<1.75-0.5*np.pi):
+        if abs(angles_diff[i]) < 0.1 and 1.4<abs(angles[i])<1.8:
             indices.append(i)
     from itertools import groupby
     z = zip(indices, indices[1:])
@@ -251,18 +254,14 @@ def selectEllipsesRange(angles):
 #        slices = ['use eyes']
     return slices, slice_angles
 
-def findPlaneFromEllipses(bone, c1, c2, slices, head_x, head_y, head_angles):
-
+def findPlaneFromEllipses(bone, c1, c2, slices, head_x, head_y, head_angles): 
     import findeyes as fer
     bone[abs(bone)<0.01]    = 0
     bone[~np.isfinite(bone)]= 0
-
-    c = (c1+c2)/2.0
-    c = np.roll(c,1)
+    coord = (c1+c2)/2.0
+    coord = np.roll(coord,1)
     if slices != []:
-
         print '\n','Using ellipses to find midplane','\n'
-
         x=[]
         y=[]
         remaining_z = range(bone.shape[2]-1, slices[-1],-1)
@@ -272,7 +271,7 @@ def findPlaneFromEllipses(bone, c1, c2, slices, head_x, head_y, head_angles):
         centroids_array = np.concatenate((np.array([head_x,head_y,slices]).T, np.array([x,y,remaining_z]).T),axis=0).T
         centroids_array[~np.isfinite(centroids_array)]=0
         centroids_array[abs(centroids_array)<0.01] = 0
-        centroids_array = rejectOutliers(centroids_array)
+        centroids_array = core.rejectOutliers(centroids_array)
         from scipy.optimize import curve_fit
         def f(x,A,B):
             return A*x + B
@@ -282,19 +281,26 @@ def findPlaneFromEllipses(bone, c1, c2, slices, head_x, head_y, head_angles):
         z = np.median(slices)
         x = A_x1*z + B_x1
         y = A_y1*z + B_y1
-        # point that the plane goes through, p
-        p = np.array([x, y, z]) # coordinates when z = mean(indices)
-        print p
+#        # point that the plane goes through, p
+#        p = np.array([x, y, z]) # coordinates when z = mean(indices)
+#        print 'p = ', p
         v1 = np.array([A_x1/2, A_y1/2, 1])
+        v1 = v1/np.sqrt((np.sum(np.power(v1,2))))
+#        print 'v1 = ', v1
         mean_angle = np.mean(head_angles)
-
-        v2a = np.array([np.cos(mean_angle+np.pi/2), np.sin(mean_angle+np.pi/2), 0])
-        v2b = (p - c)
-        v2b = v2b/(np.sum(np.power(v2b,2)))
-        v2 = (v2a+v2b)/2.0
+        v2 = np.array([np.cos(mean_angle+np.pi/2), np.sin(mean_angle+np.pi/2), 0])
+#         v2b = (p - coord)
+#         v2b = v2b/(np.sqrt(np.sum(np.power(v2b,2))))
+#         v2b = np.nan_to_num(v2b)
+#         print v2a, v2b
+#         v2 = (v2a+v2b)/2.0
         normal = np.cross(v1,v2)
+        normal = normal/np.sqrt((np.sum(np.power(normal,2))))
+#         print 'normal before invalid = ',normal
+        normal = np.nan_to_num(normal)
         # a plane is ax+by+cz = d - find d
-        d = np.dot(p,normal)
+        print coord, v1, v2
+        d = np.dot(coord,normal)
         # superpose plane onto slices
         a = normal[0]
         b = normal[1]
@@ -308,7 +314,7 @@ def findPlaneFromEllipses(bone, c1, c2, slices, head_x, head_y, head_angles):
         v1 = np.array([0,0,1])
         v2 = np.array([1,0,0])
         normal = np.cross(v1,v2)
-        d = np.dot(c,normal)
+        d = np.dot(coord,normal)
         a = normal[0]
         b = normal[1]
         c = normal[2]
@@ -365,44 +371,49 @@ def visualiseSingle(rotatedBone, a,b,c,d, slice_no):
     py.offline.iplot(fig)
     return None
 
-def correctPlaneParams(angle1, angle2, n, c1, c2):
-    def rotationMatrix(axis, theta):
-        """
-        Return the rotation matrix associated with counterclockwise rotation about
-        the given axis by theta radians.
-        """
-        axis = np.asarray(axis)
-        axis = axis/math.sqrt(np.dot(axis, axis))
-        a = math.cos(theta/2.0)
-        b, c, d = -axis*math.sin(theta/2.0)
-        aa, bb, cc, dd = a*a, b*b, c*c, d*d
-        bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
-        return np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
-                         [2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab)],
-                         [2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc]])
+def correctPlaneParams(angle1, angle2, n, c1, c2, arrayShape):
+    import math
     
-    theta1 = -angle1/360.*2*np.pi
-    theta2 = -angle2/360.*2*np.pi
-    
-    if abs(angle1) < 45:
-        axis = [0,0,1]
-        n1 = np.dot(rotationMatrix(axis,theta2*(1.-np.sin(theta1))), n)
-        axis = [0,-1,0]
-        normal = np.dot(rotationMatrix(axis,theta1), n1)
+    a = (-angle1-90.)/360.*2*np.pi
 
-    elif abs(angle1) > 45:
-        axis = [0,0,1]
-        n1 = np.dot(rotationMatrix(axis,theta2*(np.sin(theta1))), n)
-        axis = [0,-1,0]
-        normal = np.dot(rotationMatrix(axis,np.pi/2.-theta1), n1)
+    zrotM = np.asarray([[np.cos(a), -np.sin(a), 0.],
+                      [np.sin(a),  np.cos(a), 0.],
+                      [0.,          0.      , 1.]])  
 
+    yrotM = np.array([[np.cos(a),  0,         0],
+                      [np.sin(a),  1,         0],
+                      [-np.sin(a), 0, np.cos(a)]])  
+
+    R = np.dot(zrotM,yrotM)
+    n1 = np.dot(R, n)
+
+    a = (-angle2-90.)/360.*2*np.pi
+
+    zrotM = np.asarray([[np.cos(a), -np.sin(a), 0.],
+                      [np.sin(a),  np.cos(a), 0.],
+                      [0.,          0.      , 1.]])  
+
+    yrotM = np.array([[np.cos(a),  0,         0],
+                      [np.sin(a),  1,         0],
+                      [-np.sin(a), 0, np.cos(a)]])  
+
+    R = np.dot(zrotM,yrotM)
+    normal = np.dot(R, n1)
+    normal = normal/np.linalg.norm(normal)
     c = 0.5*(c1+c2)
-    c = np.roll(c,1)
-    d = np.dot(c,normal)
+    cc1 = np.roll(c,1)
+#    cc2 = np.roll(c,-1)
+#    dist1 = np.linalg.norm(np.array(arrayShape)-cc1)
+#    dist2 = np.linalg.norm(np.array(arrayShape)-cc2)
+#    if dist1<dist2:
+#        c = cc1
+#    else:
+#        c = cc2 # change this line if you change your mind
+    d = np.dot(c, normal)
     a = normal[0]
     b = normal[1]
     c = normal[2]
-    return a,b,c,d
     
+    return a,b,c,d, normal
     
     

cleancode/findeyes.py
@@ -63,21 +63,37 @@ def allSlices(axis_no, softtissue):
     return circlesData
 
 
-def indexedData(circlesData, numCircles):
+def indexedData(circlesData):
     # hist_data is the same size as circles_data but has an additional column (for the explicit 
     # slice number). hist_data is x,y,z,r
-    histData = np.zeros((circlesData.shape[0], circlesData.shape[1], circlesData.shape[2]+1))
- 
-    for i in range(histData.shape[0]):
+
+    histData2c = np.zeros((circlesData.shape[0], circlesData.shape[1], circlesData.shape[2]+1))
+    histData3c = histData2c
+    histData4c = histData2c
+    
+    numCircles = 2
+    for i in range(histData2c.shape[0]):
+        histData2c[i,0:numCircles, 0:2] = circlesData[i,0:numCircles, 0:2] 
+        #first 2 cols of every slice in circles data assigned to first 2 cols of 
+        # every slice in hist_data
+        histData2c[i,0:numCircles, 3] = circlesData[i,0:numCircles, 2] 
+        # 3rd col of every slice in circles data assigned to 4rd col of every slice 
+        # in hist_data (radii data)
+        histData2c[i,0:numCircles,2]= i*2 
+        # fill in index and stretch by factor of 2
         
-        histData[i,0:numCircles+1, 0:2] = circlesData[i,0:numCircles+1, 0:2] 
-        #first 2 cols of every slice in circles data assigned to first 2 cols of every slice in hist_data
-        
-        histData[i,0:numCircles+1, 3] = circlesData[i,0:numCircles+1, 2] 
-        # 3rd col of every slice in circles data assigned to 4rd col of every slice in hist_data (radii data)
-        
-        histData[i,0:numCircles+1,2]= i*2 # fill in index and stretch by factor of 2
-    return histData
+    numCircles = 3
+    for i in range(histData3c.shape[0]):
+        histData3c[i,0:numCircles, 0:2] = circlesData[i,0:numCircles, 0:2] 
+        histData3c[i,0:numCircles, 3] = circlesData[i,0:numCircles, 2] 
+        histData3c[i,0:numCircles,2]= i*2 
+    numCircles = 4
+    for i in range(histData4c.shape[0]):
+        histData4c[i,0:numCircles, 0:2] = circlesData[i,0:numCircles, 0:2] 
+        histData4c[i,0:numCircles, 3] = circlesData[i,0:numCircles, 2] 
+        histData4c[i,0:numCircles,2]= i*2 
+
+    return histData2c, histData3c, histData4c
 
 def reshape(hist_data0, hist_data1, hist_data2):
         # if axis_no is 0: 1st column is z, 2nd column is y, 3rd column is x. 
@@ -110,17 +126,19 @@ def circlesData(softtissue, numCircles):
     """ Fixes hist_data dimensions """
     
     circlesData0 = allSlices(0, softtissue) # axis 0 circle detection
-    histData0 = indexedData(circlesData0, numCircles)
+    histData02c, histdata03c, histdata04c = indexedData(circlesData0)
 
     circlesData1 = allSlices(1, softtissue) # axis 1 circle detection
-    histData1 = indexedData(circlesData1, numCircles)
+    histData12c, histdata13c, histdata14c = indexedData(circlesData1)
 
     circlesData2 = allSlices(2, softtissue) # axis 1 circle detection
-    histData2 = indexedData(circlesData2, numCircles)
+    histData22c, histdata23c, histdata24c = indexedData(circlesData2)
     
-    histData = reshape(histData0,histData1,histData2)
+    histData2 = reshape(histData02c,histData12c,histData22c)
+    histData3 = reshape(histdata03c, histdata13c, histdata23c)
+    histData4 = reshape(histdata04c, histdata14c, histdata24c)
  
-    return histData
+    return histData2, histData3, histData4
 
 def hist3d(hist_data):
     H, edges = np.histogramdd(hist_data[:, 0:3]) 
@@ -157,9 +175,15 @@ def hist3dAll(softtissue):
     
     # circle_num should always be greater than 1. circle_num = 1 means 2 circles being picked out. 
     
-    histData2 = circlesData(softtissue, 1) # 2c = 2 circles
-    histData3 = circlesData(softtissue, 2) 
-    histData4 = circlesData(softtissue, 3) 
+    histData2, histData3, histData4 = circlesData(softtissue, 1) # 2c = 2 circles
+#    print '\n','Circle detection for 2 circles complete','\n'
+
+#    histData3 = circlesData(softtissue, 2) 
+#    print '\n','Finding eyes part 2 of 3 complete','\n'
+
+#    histData4 = circlesData(softtissue, 3)
+#    print '\n','Finding eyes part 3 of 3 complete','\n'
+
     data2c, H2c, edges2c = hist3d(histData2)
     data3c, H3c, edges3c = hist3d(histData3)
     data4c, H4c, edges4c = hist3d(histData4)
@@ -291,47 +315,37 @@ def checkcoords(c1, c2, softtissue):
     
     return None
 
-def anglesFromEyes(c1,c2):
+def anglesFromEyes(c1,c2, arrayShape):
     # point that the plane goes through, p
-    p = (c1+c2)/2
-    normal = (c2-c1)
-    normal = normal/np.sum(np.power(normal,2))
-    # a plane is ax+by+cz = d - find d
-    d = np.dot(p,normal)
-    # superpose plane onto slices
-    a = normal[0]
-    b = normal[1]
-    c = normal[2]
-    # plane equation is ax+by+cz = d
-
-    slice_no = 1
-
-    if a != 0:
-        adj = (d-b*slice_no-c)/a - (d-b*slice_no)/a # opp is 1
-    else:
-        adj = 0
-    if adj == 0:
-        angle1rad = 0
-    else:
-        angle1rad = np.arctan(1/adj)
-    angle1 = angle1rad/(2*np.pi)*360
+    c = 0.5*(c1+c2)
+    c1 = np.roll(c1,1)
+    c2 = np.roll(c2,1)
+#    dist1 = np.linalg.norm(np.array(arrayShape)-cc1)
+#    dist2 = np.linalg.norm(np.array(arrayShape)-cc2)
+#    if dist1<dist2:
+#        c1 = np.roll(c1, 1)
+#        c2 = np.roll(c2, 1)
+#    else:
+#        c1 = np.roll(c1, -1)
+#        c2 = np.roll(c2, -1)    
+    normal = (c1-c2)
+    normal = normal/np.linalg.norm(normal)
     
-    if a != 0:
-        opp = (d-c*slice_no-b)/a - (d-c*slice_no)/a # adj is 1
-    else:
-        opp = 0
-    angle2rad = np.arctan(opp)
-    angle2 = angle2rad/(2*np.pi)*360
+    zaxis = np.array([0,0,1])
+    cosangle = np.dot(normal, zaxis)
+    angle = np.arccos(cosangle)
+    angle1 = angle*360/np.pi/2.
+
+    xaxis = np.array([0,1,0])
+    cosangle = np.dot(normal, xaxis)
+    angle = np.arccos(cosangle)
+    angle2 = angle*360/np.pi/2.
+    
     return angle1, angle2 #angles are in degrees
 
 def correctSkews(angle1, angle2, array):
     from scipy.ndimage.interpolation import rotate
-    if abs(angle1) < 45:
-        rotated1 = rotate(array, angle1, mode='nearest', axes=(2,0))
-        angle1rad = angle1/360*2*np.pi
-        rotated2 = rotate(rotated1, angle2*(1-np.sin(angle1rad)),mode='nearest', axes=(0,1)) #  can add back in
-    elif abs(angle1) > 45:
-        rotated1 = rotate(array, 90-angle1, mode='nearest', axes=(2,0))
-        angle1rad = angle1/360*2*np.pi
-        rotated2 = rotate(rotated1, angle2*(np.sin(angle1rad)),mode='nearest', axes=(0,1)) # mode='nearest', can add back in
+    rotated1 = rotate(array, angle1, mode='nearest', axes=(0,1))
+    angle1rad = angle1/360*2*np.pi
+    rotated2 = rotate(rotated1, angle2 ,mode='nearest', axes=(2,0))
     return rotated1, rotated2

cleancode/masterscript.py
@@ -36,7 +36,7 @@ class master:
             self.scan.bone        = core.thresholdnp(self.scan.array, 1100, 1500)
             self.scan.softtissue  = core.thresholdnp(self.scan.array, 0, 80)
         elif self.path.endswith('.dcm'):
-            self.scan.array, self.scan.pixelspacing, self.scan.affine = core.dicom2np(self.path)
+            self.scan.array, self.scan.pixelspacing, self.scan.origin = core.dicom2np(self.path)
             self.scan.bone       = core.thresholdnp(self.scan.array, 1100, 1500)
             self.scan.softtissue = core.thresholdnp(self.scan.array, 0, 80)
         elif self.path.endswith('.nrrd'):
@@ -68,23 +68,36 @@ class master:
         angle1, angle2 = findeyes.anglesFromEyes(self.c1Reshaped,self.c2Reshaped)
         # next find a new skull array (rotatedBone) that has been rotated so it is facing up (and the centre of the eyes lie in the same plane)
         _, rotatedBone = findeyes.correctSkews(angle1, angle2, bone)
+        _, rotatedSofttissue = findeyes.correctSkews(angle1, angle2, softtissue)
         # angles, and coords (isotropic coord system) of the centre of the ellipses that are fitted to the skull. 
+        
+        print '\n','Eyes found. Find ellipses...','\n'
+
         angs, xcentroids, ycentroids = core.ellipses(rotatedBone)
         # 'unreshaping' the results, so that the midplane will fit the original array
         # the slices of interest and their corresponding angles 
-        slices, sliceAngles = core.selectellipsesrange(angs)
+        slices, sliceAngles = core.selectEllipsesRange(angs)
         # the coordinates of the centroids of the ellipses in the slices of interest
         headx = [xcentroids[i] for i in slices] 
         heady = [ycentroids[i] for i in slices] 
-        print '\n','Eyes found. Find ellipses...','\n'
+        
+        print '\n','Ellipses found. Find eyes in rotated system...','\n'
+           
+        H, edges, histData2   = findeyes.hist3dAll(rotatedSofttissue)
+        # firstEyeRange, secondEyeRange contains the geometric space of the eyes (in the coordinate system we are currently using)
+        firstEyeRange, secondEyeRange, certainty  = findeyes.ranges(H,edges)
+        # we can now find the coordinates of the center of the eyes, using the histogram data
+        self.c1ReshapedRotated,self.c2ReshapedRotated = findeyes.coords(histData2, firstEyeRange, secondEyeRange)
+        
+        print '\n','Ellipses found. Find eyes in rotated system...','\n'        
         
         # find a midplane using a combination of the eyes (for location) and the ellipse angles
-        a,b,c,d, normal = core.findPlaneFromEllipses(self.scan.bone, self.c1Reshaped, self.c2Reshaped, slices, headx, heady, sliceAngles)
+        a,b,c,d, normal = core.findPlaneFromEllipses(self.scan.bone, self.c1ReshapedRotated, self.c2ReshapedRotated, slices, headx, heady, sliceAngles)
         # put this plane back into the original coordinate system
         theta1 = angle1/360.*2*np.pi # angles in radians
         theta2 = angle2/360.*2*np.pi
         
-        a,b,c,d = core.correctPlaneParams(angle1, angle2, normal, c1, c2)
+        a,b,c,d = core.correctPlaneParams(angle1, angle2, normal, self.c1, self.c2)
         self.scan.params = a,b,c,d
         print '\n','Midplane found...','\n'
                 
@@ -106,8 +119,15 @@ class master:
         
         mask = np.swapaxes(mask, 0, 2)
         img = sitk.GetImageFromArray(mask)
-        img.CopyInformation(sitk.ReadImage(self.path))
-        midplaneMaskPath = os.path.join(os.path.split(self.path)[0], 'midplane.nii.gz')
+        
+        if self.path.endswith('.dcm'):
+            dcmimg = sitk.ReadImage(self.path)
+            img.SetDirection(dcmimg.GetDirection())
+            img.SetOrigin(self.scan.origin)
+            img.SetSpacing(self.scan.pixelspacing)
+        else:
+            img.CopyInformation(sitk.ReadImage(self.path))
+        midplaneMaskPath = os.path.join(os.path.split(self.path)[0], 'midplane.mha')
         sitk.WriteImage(img, midplaneMaskPath)
         print '\n','Midplane mask saved at ', midplaneMaskPath,'\n'
 